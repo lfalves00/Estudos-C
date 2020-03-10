5441cf19b4f7fd56224eca1396e55f540380c864
lipesk <lf@lcda.com.br>
2020-03-10 16:33:38 -0300

    Add files via upload

---
+++ escape sequence.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	/*
+	
+	1) \n -> para realizar quebra de linha
+	2) \t -> para realizar um tab no momento em que estamos imprimindo
+	3) \\ -> imprimir uma barra invertida
+	4) \' -> imprimir uma aspas simples
+	5) \" -> imprimir uma aspas dupla
+	6) \o -> sinalizar fim de texto
+	
+	*/
+	return 0;
+}
